094b655c2ee41e19b0efe6248a6216927bdabae6
sk1llpy <abdullohnugmonovone1@gmail.com>
2024-09-22 11:43:24 +0500

    done

--- database.py
+++ database.py
@@ -3,83 +3,146 @@ import datetime
 
 
 async def create_table():
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
 
-        await cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-                                user_id BIGINT,
-                                user_type VARCHAR(255),
-                                full_name VARCHAR(255),
-                                phone_number VARCHAR(255)
-                            )""")
-        await cursor.execute("""CREATE TABLE IF NOT EXISTS announcements (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                from_user_id BIGINT,
-                                created_at TIMESTAMP,
-                                title VARCHAR(255),
-                                description TEXT,
-                                status VARCHAR(255) DEFAULT 'noone_accepted'
-                            )""")
-        await cursor.execute("""CREATE TABLE IF NOT EXISTS accepted (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                user_id BIGINT,
-                                announcement_id INT,
-                                status VARCHAR(255)
-                            )""")
-        await connect.commit()
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id BIGINT,
+                user_type VARCHAR(255),
+                full_name VARCHAR(255),
+                phone_number VARCHAR(255)
+            )
+        """)
 
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS announcements (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                from_user_id BIGINT,
+                created_at TIMESTAMP,
+                title VARCHAR(255),
+                description TEXT
+            )
+        """)
+
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS answers (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id BIGINT,
+                announcement_id INT
+            )
+        """)
+
+        await connection.commit()
         return None
-    
+
+
 async def get_user(user_id: int):
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
 
-        user = await (await cursor.execute(f"""SELECT * FROM users WHERE user_id = {user_id}""")).fetchone()
-        await connect.commit()
+        user = await (await cursor.execute(
+            f"SELECT * FROM users WHERE user_id = {user_id}"
+        )).fetchone()
 
+        await connection.commit()
         return user
+
 
 async def create_user(user_id: int, phone_number: str, full_name: str):
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
 
-        await cursor.execute(f"""INSERT INTO users (user_id, phone_number, full_name, user_type) 
-                         VALUES ({user_id}, '{phone_number}', '{full_name}', 'user')""")
-        user = await (await cursor.execute(f"""SELECT * FROM users WHERE user_id = {user_id}""")).fetchone()
-        await connect.commit()
+        await cursor.execute(
+            f"""
+            INSERT INTO users (user_id, phone_number, full_name, user_type)
+            VALUES ({user_id}, '{phone_number}', '{full_name}', 'user')
+            """
+        )
 
+        user = await (await cursor.execute(
+            f"SELECT * FROM users WHERE user_id = {user_id}"
+        )).fetchone()
+
+        await connection.commit()
         return user
-    
+
 
 async def create_announcement(from_user_id: int, title: str, description: str):
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
         dt = datetime.datetime.now()
-        await cursor.execute(f"""INSERT INTO announcements (from_user_id, created_at, title, description)
-                         VALUES ({from_user_id}, '{dt}', '{title}', '{description}')""")
-        announcement = await (await cursor.execute(f"""SELECT * FROM announcements 
-                                                  WHERE from_user_id = {from_user_id} 
-                                                  ORDER BY id DESC LIMIT 1""")).fetchone()
-        await connect.commit()
 
+        await cursor.execute(
+            f"""
+            INSERT INTO announcements (from_user_id, created_at, title, description)
+            VALUES ({from_user_id}, '{dt}', '{title}', '{description}')
+            """
+        )
+
+        announcement = await (await cursor.execute(
+            f"""
+            SELECT * FROM announcements
+            WHERE from_user_id = {from_user_id}
+            ORDER BY id DESC LIMIT 1
+            """
+        )).fetchone()
+
+        await connection.commit()
         return announcement
 
 
 async def get_users_list():
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
 
-        users = await (await cursor.execute(f"""SELECT * FROM users WHERE user_type = 'user'""")).fetchall()
-        await connect.commit()
+        users = await (await cursor.execute(
+            "SELECT * FROM users WHERE user_type = 'user'"
+        )).fetchall()
 
+        await connection.commit()
         return users
-    
+
 
 async def get_announcement(announcement_id: int):
-    async with aiosqlite.connect('main.db') as connect:
-        cursor = await connect.cursor()
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
 
-        announcement = await (await cursor.execute(f"""SELECT * FROM announcements WHERE id = {announcement_id}""")).fetchone()
-        await connect.commit()
+        announcement = await (await cursor.execute(
+            f"SELECT * FROM announcements WHERE id = {announcement_id}"
+        )).fetchone()
 
+        await connection.commit()
         return announcement
+
+
+async def get_answer(answer_id: int):
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
+
+        answer = await (await cursor.execute(
+            f"SELECT * FROM answers WHERE id = {answer_id}"
+        )).fetchone()
+
+        await connection.commit()
+        return answer
+
+
+async def create_answer(user_id: int, announcement_id: int):
+    async with aiosqlite.connect('main.db') as connection:
+        cursor = await connection.cursor()
+
+        await cursor.execute(
+            f"INSERT INTO answers (user_id, announcement_id) VALUES ({user_id}, {announcement_id})"
+        )
+
+        answer = await (await cursor.execute(
+            f"""
+            SELECT * FROM answers
+            WHERE user_id = {user_id} AND announcement_id = {announcement_id}
+            ORDER BY id DESC LIMIT 1
+            """
+        )).fetchone()
+
+        await connection.commit()
+        return answer
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import asyncio
 import logging
 import database as db
-
 from aiogram import Dispatcher, Bot, types, enums, html, F
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup, 
-                           InlineKeyboardButton, InlineKeyboardMarkup)
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import CommandStart, StateFilter
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 
-
 logging.basicConfig(level=logging.INFO)
-
 
 
 class RegisterState(StatesGroup):
     full_name = State()
     phone_number = State()
 
+
 class CreateAnnouncement(StatesGroup):
     title = State()
     description = State()
+
 
 class AnswerAnnouncement(StatesGroup):
     answer = State()
@@ -30,47 +33,45 @@ class AnswerAnnouncement(StatesGroup):
 
 BOT_TOKEN = '7224050581:AAHrb6hnOAZ5OlUudb0dsmz-uin6DxtHNZU'
 bot = Bot(
-    token = BOT_TOKEN, 
-    default = DefaultBotProperties(
-        parse_mode = enums.ParseMode.HTML
-    )
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=enums.ParseMode.HTML),
 )
 
-
-dp = Dispatcher(storage = MemoryStorage())
+dp = Dispatcher(storage=MemoryStorage())
 storage = dp.storage
 
 
 @dp.message(CommandStart())
 async def command_start(message: types.Message, state: FSMContext):
     user = await db.get_user(message.from_user.id)
-
     if user:
         if user[1] == 'user':
             await message.answer(
-                text = f"Salom {message.from_user.first_name} 👋",
-                reply_markup=types.ReplyKeyboardRemove()
+                text=f"Salom, <b>{message.from_user.first_name}</b> 👋",
+                reply_markup=types.ReplyKeyboardRemove(),
             )
         elif user[1] == 'business':
             await message.answer(
-                text = f"Salom {message.from_user.first_name}!\n\n" + (
-                    html.italic("Kerakli bo'limni  tanlang 👇")
-                ),
-                reply_markup = types.ReplyKeyboardMarkup(
-                    resize_keyboard = True,
-                    one_time_keyboard = True,
-                    keyboard = [
+                text=f"Salom, <b>{message.from_user.first_name}</b>!\n\n"
+                + html.italic("Kerakli bo'limni tanlang 👇"),
+                reply_markup=types.ReplyKeyboardMarkup(
+                    resize_keyboard=True,
+                    one_time_keyboard=True,
+                    keyboard=[
                         [
-                            KeyboardButton(text = "🚀 E'lon qo'shish", callback_data = "create_annoucment")
+                            KeyboardButton(
+                                text="🚀 E'lon qo'shish",
+                                callback_data="create_annoucement",
+                            )
                         ]
-                    ]
-                )
+                    ],
+                ),
             )
     else:
         await state.set_state(RegisterState.full_name)
         await message.answer(
-            text = "Ism-familyangizni kiriting!",
-            reply_markup = types.ReplyKeyboardRemove()
+            text="<i>Ism-familyangizni kiriting!</i>",
+            reply_markup=types.ReplyKeyboardRemove(),
         )
 
 
@@ -78,17 +79,21 @@ async def command_start(message: types.Message, state: FSMContext):
 async def full_name_handler(message: types.Message, state: FSMContext):
     await state.update_data(full_name=message.text)
     await state.set_state(RegisterState.phone_number)
-    
-    await message.answer("Telefon raqamingizni yuboring!",
+    await message.answer(
+        "<i>Telefon raqamingizni yuboring!</i>",
         reply_markup=ReplyKeyboardMarkup(
             resize_keyboard=True,
             one_time_keyboard=True,
             keyboard=[
                 [
-                    KeyboardButton(text="Telefon raqamni yuborish", request_contact=True)
+                    KeyboardButton(
+                        text="📱 Telefon raqamni yuborish",
+                        request_contact=True,
+                    )
                 ]
-            ]
-        ))
+            ],
+        ),
+    )
 
 
 @dp.message(F.content_type == 'contact', StateFilter(RegisterState.phone_number))
@@ -96,119 +101,188 @@ async def contact_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
     full_name = data['full_name']
     phone_number = message.contact.phone_number
-
     await db.create_user(message.from_user.id, phone_number, full_name)
-
-    await message.answer("Siz muvaffaqiyatli ro'yhatdan o'tdingiz!",
-        reply_markup=types.ReplyKeyboardRemove())
-    
+    await message.answer(
+        "<b>Siz muvaffaqiyatli ro'yhatdan o'tdingiz!</b>",
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
     await state.clear()
 
 
 @dp.message(F.text == '🚀 E\'lon qo\'shish')
-async def create_annoucment_handler(message: types.Message, state: FSMContext):
+async def create_annoucement_handler(message: types.Message, state: FSMContext):
     user = await db.get_user(message.from_user.id)
-
     if user[1] == 'business':
         await state.set_state(CreateAnnouncement.title)
-        await message.answer(text="E'lon qo'shish uchun nom kiriting!", reply_markup=types.ReplyKeyboardRemove())
+        await message.answer(
+            text="<i>E'lon qo'shish uchun nom kiriting!</i>",
+            reply_markup=types.ReplyKeyboardRemove(),
+        )
 
 
 @dp.message(F.content_type == 'text', StateFilter(CreateAnnouncement.title))
 async def announcement_title_handler(message: types.Message, state: FSMContext):
     await state.update_data(title=message.text)
     await state.set_state(CreateAnnouncement.description)
-    
-    await message.answer("E'lon haqida to'liq ma'umot yozing!")
+    await message.answer("<i>E'lon haqida to'liq ma'lumot yozing!</i>")
 
 
 @dp.message(F.content_type == 'text', StateFilter(CreateAnnouncement.description))
-async def announcement_description_handler(message: types.Message, state: FSMContext):
+async def announcement_description_handler(
+    message: types.Message, state: FSMContext
+):
     data = await state.get_data()
     title = data['title']
     description = message.text
-    
-    announcement = await db.create_announcement(from_user_id=message.from_user.id, title=title, description=description)
-    announcement_msg = await message.answer(f"""⚠️ E'LON ⚠️
-                                       
-<b>{title}</b>
-<i>E'lon xaqida: {description}</i>""")
-    await message.answer("E'lon muvaffaqiyatli yaratildi!", reply_to_message_id=announcement_msg.message_id)
-    await state.clear()
+    announcement = await db.create_announcement(
+        from_user_id=message.from_user.id, title=title, description=description
+    )
+    announcement_msg = await message.answer(
+        f"""⚠️ <b>E'LON</b> ⚠️
 
+<b>{title}</b>
+<i>E'lon haqida: {description}</i>"""
+    )
+    await message.answer(
+        "<b>E'lon muvaffaqiyatli yaratildi!</b>",
+        reply_to_message_id=announcement_msg.message_id,
+    )
+    await state.clear()
     users = await db.get_users_list()
     for user in users:
-        await bot.send_message(user[0], f"""⚠️ E'LON ⚠️
-                                       
+        await bot.send_message(
+            user[0],
+            f"""⚠️ <b>E'LON</b> ⚠️
+
 <b>{title}</b>
-<i>E'lon xaqida: {description}</i>""", reply_markup=InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(text="Javob yozish", callback_data=f'announcement_{announcement_msg.message_id}_{announcement[0]}')
-            ]
-        ]
-    ))
+<i>E'lon haqida: {description}</i>
+            """,
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text="✍️ Javob yozish",
+                            callback_data=f'announcement_{announcement_msg.message_id}_{announcement[0]}',
+                        )
+                    ]
+                ]
+            ),
+        )
 
 
 @dp.callback_query(F.data.startswith('announcement_'))
-async def answer_announcement_handler(call: types.CallbackQuery, state: FSMContext):
+async def answer_announcement_handler(
+    call: types.CallbackQuery, state: FSMContext
+):
     announcement_id = call.data.split("_")[-1]
     announcement_msg_id = call.data.split("_")[1]
     announcement = await db.get_announcement(announcement_id)
-
     await state.set_state(AnswerAnnouncement.answer)
-    await state.update_data(announcement=announcement, 
-                            message_id=call.message.message_id, business_account_message_id=announcement_msg_id)
+    await state.update_data(
+        announcement=announcement,
+        message_id=call.message.message_id,
+        business_account_message_id=announcement_msg_id,
+    )
     await call.message.edit_reply_markup()
     await call.message.answer(
-        f"Javob yozing!\n\n<i>Siz ushbu e'longa javob yozyapsiz</i>",
-        reply_to_message_id=call.message.message_id)
+        f"<i>Javob yozing!</i>\n\n<i>Siz ushbu e'longa javob yozyapsiz</i>",
+        reply_to_message_id=call.message.message_id,
+    )
 
 
 @dp.message(F.content_type == 'text', StateFilter(AnswerAnnouncement.answer))
-async def announcement_title_handler(message: types.Message, state: FSMContext):
+async def answer_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
     message_id = data['message_id']
     announcement = data['announcement']
     business_account_message_id = data['business_account_message_id']
     user = await db.get_user(message.from_user.id)
-    
+    answer = await db.create_answer(
+        user_id=message.from_user.id, announcement_id=announcement[0]
+    )
     try:
-        await bot.send_message(chat_id=announcement[1], text=f"""E'LON JAVOBI!
+        await bot.send_message(
+            chat_id=announcement[1],
+            text=f"""<b>E'LON JAVOBI 👇</b>
 
-{message.text}""", reply_to_message_id=business_account_message_id, reply_markup=InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text=f"{user[2]}", url=f"https://t.me/{message.from_user.username}")],
-                [InlineKeyboardButton(text=f"Qabul qilish ✅", callback_data=f"accept_user")],
-    ]
-))
+{message.text}""",
+            reply_to_message_id=business_account_message_id,
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text=f"{user[2]}",
+                            url=f'https://t.me/{message.from_user.username}',
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="Qabul qilish ✅",
+                            callback_data=f'accept_answer_{answer[0]}',
+                        )
+                    ]
+                ]
+            )
+        )
     except:
-        await bot.send_message(chat_id=announcement[1], text=f"""E'LON JAVOBI!
+        await bot.send_message(
+            chat_id=announcement[1],
+            text=f"""<b>E'LON JAVOBI 👇</b>
 
-{message.text}""", reply_markup=InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text=f"{user[2]}", url=f"https://t.me/{message.from_user.username}")],
-                [InlineKeyboardButton(text=f"Qabul qilish ✅", callback_data=f"accept_user")],
-    ]
-))
+{message.text}""",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text=f"{user[2]}",
+                            url=f'https://t.me/{message.from_user.username}',
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="Qabul qilish ✅",
+                            callback_data=f'accept_answer_{answer[0]}',
+                        )
+                    ]
+                ]
+            )
+        )
     await bot.edit_message_reply_markup(
-        message_id=message_id, chat_id=message.chat.id,
+        message_id=message_id,
+        chat_id=message.chat.id,
         reply_markup=InlineKeyboardMarkup(
             inline_keyboard=[
                 [
-                    InlineKeyboardButton(text="Javob yozish", callback_data=f'announcement_{announcement[0]}')
+                    InlineKeyboardButton(
+                        text="✍️ Javob yozish",
+                        callback_data=f'announcement_{business_account_message_id}_{announcement[0]}',
+                    )
                 ]
             ]
-        ))
-    
-    await message.answer("Javob yuborildi!")
+        ),
+    )
+    await message.answer("<b>Javob yuborildi ✅</b>")
     await state.clear()
 
-# ------------------------------------------
 
+@dp.callback_query(F.data.startswith('accept_answer_'))
+async def accept_answer_handler(call: types.CallbackQuery, state: FSMContext):
+    answer_id = call.data.split("_")[-1]
+    answer = await db.get_answer(answer_id)
+    announcement = await db.get_announcement(answer[2])
+    await bot.send_message(
+        chat_id=answer[1],
+        text=f"Siz qabul qildingiz ✅\n\nE'lon: <b>{announcement[3]}</b>",
+    )
+    await call.message.edit_reply_markup()
+    await call.message.answer("<i>Xabar foydalanuvchiga yuborildi ✅</i>", reply_to_message_id=call.message.message_id)
+
+
+# ------------------------------------------
 
 async def main():
     await db.create_table()
     await dp.start_polling(bot)
+
 
 asyncio.run(main())
